5f3be09167de6dc1a1b0548403cea2e821ec7aef
jojolin <ljzjojop@126.com>
2015-06-19 10:11:01 +0800

    revert init-el-get.el

--- lisp/init-el-get.el
+++ lisp/init-el-get.el
@@ -9,16 +9,16 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 ;; when i installed, it's not available in elpa for network reason.
-;; (unless (require 'el-get nil 'noerror)
-;;   (require 'package)
-;;   (add-to-list 'package-archives
-;;                '("melpa" . "http://melpa.org/packages/"))
-;;   (package-refresh-contents)
-;;   (package-initialize)
-;;   (package-install 'el-get)
-;;   (require 'el-get))
+;; if through elpa, it may be installed into ~/.emacs.d/elpa/el-get-xxx
+(unless (require 'el-get nil 'noerror)
+  (require 'package)
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.org/packages/"))
+  (package-refresh-contents)
+  (package-initialize)
+  (package-install 'el-get)
+  (require 'el-get))
 
-(require 'el-get)
 ;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/user-recipes")
 (el-get 'sync)
 (el-get 'wait)
